b17addec3bb27a9f0e07c14dcb85fabf86ee9ca5
Kemal Zebari <kemalzebra@gmail.com>
2023-04-20 19:02:01 -0700

    feat(users): Add friendship relationship

--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from chatter.settings import AUTH_USER_MODEL
 
 
 class ChatterUser(AbstractUser):
-    pass
+    friends = models.ManyToManyField("self", through="Friendship")
+
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    )
+    to_user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    )
+    date_started = models.DateField(auto_now_add=True)
