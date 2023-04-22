8c52f50bc7a64e69f3642c813b1cff0d1d964bf9
Kemal Zebari <kemalzebra@gmail.com>
2023-04-22 12:48:44 -0700

    feat(users): Add a `FriendRequest` model
    Also add a unique constraint to `Friendship`.

--- users/models.py
+++ users/models.py
@@ -16,3 +16,28 @@ class Friendship(models.Model):
         AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
     )
     date_started = models.DateField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["from_user", "to_user"],
+                name="a_one_way_friendship_is_unique",
+            ),
+        ]
+
+
+class FriendRequest(models.Model):
+    requester = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_requests"
+    )
+    addressee = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="pending_requests"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["requester", "addressee"],
+                name="requester_addressee_unique",
+            ),
+        ]
