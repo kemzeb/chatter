f5eb566b29c5efeeb95023eeefdf1fba525d7275
Kemal Zebari <kemalzebra@gmail.com>
2023-04-23 16:55:27 -0700

    refactor(frontend): Reorganize App.js and index.js
    Also remove an unneeded HTML element.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import RequireAuth from './utils/RequireAuth';
-import './App.css';
 import { AuthProvider } from './utils/AuthContext';
 
 function App() {
   return (
-    <main className="main">
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/dashboard"
-              element={
-                <RequireAuth>
-                  <Dashboard />
-                </RequireAuth>
-              }
-            />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </main>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
