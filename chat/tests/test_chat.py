30485acb35ec1bb48e2bb125b4d9b084029b16bc
Kemal Zebari <kemalzebra@gmail.com>
2023-05-11 14:27:30 -0700

    test(chat): Merge test_chat_views.py & test_chat_consumers.py

--- chat/tests/test_chat.py
+++ chat/tests/test_chat.py
@@ -2,13 +2,54 @@ import json
 
 import pytest
 from channels.db import database_sync_to_async
+from channels.layers import get_channel_layer
+from channels.testing import WebsocketCommunicator
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.serializers import DateTimeField
 from rest_framework.test import APIClient
 
 from chat.models import ChatGroup, ChatMessage
-from chatter.utils import EventName
+from chatter.asgi import application
+from chatter.utils import EventName, get_group_name
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+class TestChatConsumer:
+    async def test_with_anonymous_user(self, origin_header):
+        token = "invalid_jwt_access_token"
+        communicator = WebsocketCommunicator(
+            application,
+            f"/ws/chat?token={token}",
+            headers=[origin_header],
+        )
+
+        connected, _ = await communicator.connect()
+        assert connected is False, "Should not allow unauthenticated users."
+
+        # NOTE: Even though the connection should fail, warnings will appear if this
+        # line is not included.
+        await communicator.disconnect()
+
+    async def test_on_connect(self, communicator1_without_handling, user_1):
+        connected, _ = await communicator1_without_handling.connect()
+        assert connected
+
+        chat_group = await ChatGroup.objects.aget(owner=user_1, name="Precursors rule")
+        channel_layer = get_channel_layer()
+        assert channel_layer is not None
+        await channel_layer.group_send(
+            get_group_name(chat_group.pk),
+            {
+                "type": "handle_create_message",
+            },
+        )
+
+        response = await communicator1_without_handling.receive_json_from()
+        assert response["event_type"] == str(EventName.GROUP_MESSAGE)
+
+        await communicator1_without_handling.disconnect()
 
 
 @pytest.mark.django_db
