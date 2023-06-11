9f0a3898e5964c006b8891ac709d81c5ac647ed1
Kemal Zebari <kemalzebra@gmail.com>
2023-06-11 02:11:11 -0700

    build: Switch to using postgres as default backend

--- chatter/settings.py
+++ chatter/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = "chatter.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
